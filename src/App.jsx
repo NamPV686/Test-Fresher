e4237a7aa9816086aa627879a29cb955265e6b26
NamPV686 <nampv686@gmail.com>
2023-05-31 08:54:27 +0700

    4.2. chia layout va project structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import LoginPage from "./pages/login";
+import ContactPage from "./pages/contact";
+import BookPage from "./pages/book";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 const Layout = () => {
   return(
-    <>
-      main page
-    </>
+    <div className="layout-app">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
   )
 }
 
@@ -16,7 +24,18 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <div>404 not found</div>
+    errorElement: <div>404 not found</div>,
+    children: [
+      {index: true, element: <Home />},
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "book",
+        element: <BookPage />,
+      },
+    ],
   },
   {
     path: "/login",
