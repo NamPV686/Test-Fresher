f8ecf7b26b3ad12515fdf1cf42b3bad02a45c51b
NamPV686 <nampv686@gmail.com>
2023-05-31 15:31:37 +0700

    #13.Axios customize

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Checkbox, Divider, Form, Input } from 'antd';
+import './register.scss'
 
 const onFinish = (values) => {
     console.log('Success:', values);
@@ -12,27 +13,26 @@ const onFinish = (values) => {
 const RegisterPage = () => {
     return(
         <div className='register-page' style={{padding: '50px'}}>
-            <h3 style={{textAlign: 'center'}}>Đăng ký người dùng mới</h3>
+            <h3 style={{textAlign: 'center'}}>Đăng Ký Tài Khoản</h3>
             <Divider />
             <Form
                 name="basic"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: 600, margin: '0 auto' }}
-                initialValues={{ remember: true }}
+                // style={{ maxWidth: 600, margin: '0 auto' }}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
                 autoComplete="off"
             >
                 <Form.Item
+                labelCol={{ span: 24 }}
                 label="FullName"
-                name="fullname"
+                name="fullName"
                 rules={[{ required: true, message: 'Please input your fullname!' }]}
                 >
                 <Input />
                 </Form.Item>
 
                 <Form.Item
+                labelCol={{ span: 24 }}
                 label="Email"
                 name="email"
                 rules={[{ required: true, type: 'email', message: 'Please input your email!' }]}
@@ -41,6 +41,7 @@ const RegisterPage = () => {
                 </Form.Item>
 
                 <Form.Item
+                labelCol={{ span: 24 }}
                 label="Password"
                 name="password"
                 rules={[{ required: true, message: 'Please input your password!' }]}
@@ -49,6 +50,7 @@ const RegisterPage = () => {
                 </Form.Item>
 
                 <Form.Item
+                labelCol={{ span: 24 }}
                 label="Phone"
                 name="phone"
                 rules={[{ required: true, message: 'Please input your phone!' }]}
@@ -56,8 +58,8 @@ const RegisterPage = () => {
                 <Input />
                 </Form.Item>
 
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
+                <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+                <Button type="primary" htmlType="submit" loading={false}>
                     Submit
                 </Button>
                 </Form.Item>
